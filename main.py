8fa083bcce5a4defc87a9693bdf223782e1dee7f
Sean Clifford <seancliff01@gmail.com>
2023-10-06 14:49:17 -0400

    added new note

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import openai
 import dotenv
 import os
 import queue
+import sys
 
 from audio.recorder import AudioRecorder
 from audio.transcriber import Transcriber
 from chatgpt.completions import ChatGPT
+from notes import Note, NoteStorage
 
 
 dotenv.load_dotenv()
@@ -14,9 +16,10 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def main():
     q = queue.Queue()
+    note = NoteStorage("notes").get_note("note 1")
     recorder = AudioRecorder(q, record_length=15)
     transcriber = Transcriber("recordings/recording.wav")
-    chatgpt = ChatGPT()
+    chatgpt = ChatGPT(note)
     recorder.start()
     
     try:
@@ -26,8 +29,13 @@ def main():
             print(text)
             if len(text.split()) < 4:
                 continue
+            note.add_transcript(text)
+
             response = chatgpt.chat_completion(text)
-            print(response)
+            print("response:", response)
+            note.add_completion(response)
+
+            note.save()
     except KeyboardInterrupt:
         pass
 
